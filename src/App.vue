0b00b4e69cc6b270b3f919a363604e995d533885
MMJJDXJ <lj2801045990@163.com>
2020-06-19 08:57:47 +0800

    第二次导航栏动画提交

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,25 @@
   <div id="app">
     <router-view></router-view>
 
-    <!-- 天猫头部 -->
+    <!-- 天猫导航栏-->
     <div class="tm-header-lj">
-      <a href="https://www.tmall.com/wow/car/act/navtab?pos=5&acm=201803150.1003.2.3323494&scm=1003.2.201803150.OTHER_1523452695069_3323494#mui-nav-category">
-        <img src="./images/three-heng.png" alt />
-      </a>
-      <img class="header-tmall-lj" src="./images/TMALL.png" alt />
-      <span @click="enterBtn" class="header-enter-lj">登录</span>
-    </div>
+      <div class="header-top-lj">
+        <div class="header-threeheng-lj">
+          <a href="https://www.tmall.com/wow/car/act/navtab?pos=5&acm=201803150.1003.2.3323494&scm=1003.2.201803150.OTHER_1523452695069_3323494#mui-nav-category"><img src="./images/three-heng.png" alt /></a>
+        </div>
 
-    <!-- 天猫搜索框 -->
-    <div @click="searchBtn" class="tm-search-lj">
-      <div class="search-input-lj">
-        <img src="./images/search.png" alt />
-        <span>搜索商品、品牌</span>
+        <div class="header-tmall-lj">
+          <img src="./images/TMALL.png" alt />
+        </div>
+
+        <div @click="enterBtn" class="header-enter-lj">登录</div>
+      </div>
+
+      <div class="tm-search-lj">
+        <div class="search-input-lj">
+          <img src="./images/search.png" alt />
+          <span>搜索商品、品牌</span>
+        </div>
       </div>
     </div>
 
@@ -61,18 +66,9 @@
       </li>
     </ul>
 
-
     <!-- 天猫轮播图 -->
 
-
-
-
-
-
-
-
-
-    <div style="height:1000px"></div>
+    <div style="height:1888px"></div>
     <button @click="cartBtn">购物车router</button>
     <button @click="productBtn">商品详情router</button>
   </div>
@@ -80,29 +76,81 @@
 
 <script>
 export default {
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
   methods: {
+    // 路由
     cartBtn() {
       this.$router.push("cartPath");
     },
-
     productBtn() {
       this.$router.push("productPath");
     },
-
     classifyBtn() {
       this.$router.push("classifyPath");
     },
-
     enterBtn() {
       this.$router.push("enterPath");
     },
-
-    searchBtn(){
+    searchBtn() {
       this.$router.push("searchPath");
-    }
+    },
+
+    // 导航栏过渡动画
+    leave() {
+      let left = document.querySelector(".header-threeheng-lj");
+      let right = document.querySelector(".header-enter-lj");
+      let tmall = document.querySelector(".header-tmall-lj");
+      let search = document.querySelector(".tm-search-lj");
+      let input = document.querySelector(".search-input-lj");
+      let header = document.querySelector(".tm-search-lj");
+
+      left.style.transform = "translateY(9px)";
+      right.style.transform = "translateY(8px)";
+      tmall.style.transform = "scale(0)";
+      tmall.style.height = "0px";
+      input.style.transform = " scaleX(0.8) translateY(-30px)";
+      header.style.height = "7px";
+    },
+
+    enter() {
+      let left = document.querySelector(".header-threeheng-lj");
+      let right = document.querySelector(".header-enter-lj");
+      let tmall = document.querySelector(".header-tmall-lj");
+      let search = document.querySelector(".tm-search-lj");
+      let input = document.querySelector(".search-input-lj");
+      let header = document.querySelector(".tm-search-lj");
+
+      left.style.transform = "translateY(0px)";
+      right.style.transform = "translateY(0px)";
+      tmall.style.transform = "scale(1)";
+      tmall.style.height = "25px";
+      input.style.transform = " scaleX(1) translateY(0px)";
+      header.style.height = "36px";
+    },
+
+    handleScroll() {
+      var scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+
+      if (scrollTop > 10) {
+        this.leave();
+      } else {
+        this.enter();
+      }
+    },
+
+    //轮播图
+    
+
   }
 };
 </script>
+
 
 <style>
 * {
@@ -125,34 +173,56 @@ a {
   text-decoration: none;
 }
 
-/* 天猫头部 */
+/* 天猫导航栏头部 */
 .tm-header-lj {
-  height: 44px;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
   background-color: rgb(255, 0, 54);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.header-top-lj {
+  height: 44px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 0 11px;
+  padding: 0 10px;
+  transition: all 0.36s;
+}
+
+.header-threeheng-lj {
+  margin-top: 2px;
+  transition: all 0.36s;
 }
 
 .header-tmall-lj {
-  height: 20.5px;
+  transition: all 0.36s;
+}
+
+.header-tmall-lj img {
   width: 118.5px;
+  height: 20.5px;
 }
 
 .header-enter-lj {
-  color: #fff;
   font-size: 14px;
+  transition: all 0.36s;
+  color: #fff;
 }
 
-/* 天猫搜索框 */
 .tm-search-lj {
   padding: 0 10px 10px;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   background: rgb(255, 0, 54);
+  transition: all 0.36s;
 }
 
 .search-input-lj {
@@ -165,6 +235,7 @@ a {
   background: #fff;
   font-size: 14px;
   color: #bbbbbb;
+  transition: all 0.36s;
 }
 
 .search-input-lj img {
@@ -173,6 +244,7 @@ a {
 
 /* 天猫导航 */
 .nav-ul-lj {
+  margin-top: 90px;
   height: 78px;
   display: flex;
   flex-direction: row;
@@ -201,4 +273,9 @@ a {
   margin-top: 7px;
   color: black;
 }
+
+
+/* 轮播图 */
+
+
 </style>
